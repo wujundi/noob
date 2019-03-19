3b708d16e312ef2232264ced70c82d6174cab7db
wujundi <wujundimail@163.com>
2019-03-19 22:43:39 +0800

    Initial commit

---
+++ src/main/java/wujundi/Noob/NoobApplication.java
@@ -0,0 +1,13 @@
+package wujundi.Noob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoobApplication.class, args);
+	}
+
+}
