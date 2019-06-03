d4f4a8383dd7eb21737e4818a6ee8fc93e93d4df
wujundi <wujundimail@163.com>
2019-06-03 21:10:35 +0800

    2019-06-03 20:58 add web-magic extension module

---
+++ spider-extension/src/test/java/com/noob/spider/example/GithubRepoPageProcessorTest.java
@@ -0,0 +1,30 @@
+package com.noob.spider.example;
+
+import org.junit.Test;
+import com.noob.spider.ResultItems;
+import com.noob.spider.Spider;
+import com.noob.spider.Task;
+import com.noob.spider.downloader.MockGithubDownloader;
+import com.noob.spider.pipeline.Pipeline;
+import com.noob.spider.processor.example.GithubRepoPageProcessor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author code4crafter@gmail.com
+ *         Date: 16/1/19
+ *         Time: 上午7:27
+ */
+public class GithubRepoPageProcessorTest {
+
+    @Test
+    public void test_github() throws Exception {
+        Spider.create(new GithubRepoPageProcessor()).addPipeline(new Pipeline() {
+            @Override
+            public void process(ResultItems resultItems, Task task) {
+                assertThat(((String) resultItems.get("name")).trim()).isEqualTo("com/noob/spider");
+                assertThat(((String) resultItems.get("author")).trim()).isEqualTo("code4craft");
+            }
+        }).setDownloader(new MockGithubDownloader()).test("https://github.com/code4craft/webmagic");
+    }
+}
---
+++ spider-extension/src/test/java/com/noob/spider/model/samples/OschinaAnswer.java
@@ -0,0 +1,32 @@
+package com.noob.spider.model.samples;
+
+import com.noob.spider.Page;
+import com.noob.spider.Site;
+import webmagic.model.*;
+import com.noob.spider.model.annotation.ExtractBy;
+import com.noob.spider.model.annotation.HelpUrl;
+import com.noob.spider.model.annotation.TargetUrl;
+
+/**
+ * @author code4crafter@gmail.com <br>
+ */
+@TargetUrl("http://www.oschina.net/question/\\d+_\\d+*")
+@HelpUrl("http://www.oschina.net/question/*")
+@ExtractBy(value = "//ul[@class='list']/li[@class='Answer']", multi = true)
+public class OschinaAnswer implements AfterExtractor{
+
+    @ExtractBy("//img/@title")
+    private String user;
+
+    @ExtractBy("//div[@class='detail']")
+    private String content;
+
+    public static void main(String[] args) {
+        OOSpider.create(Site.me(), OschinaAnswer.class).addUrl("http://www.oschina.net/question/567527_120597").run();
+    }
+
+    @Override
+    public void afterProcess(Page page) {
+
+    }
+}
